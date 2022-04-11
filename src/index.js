0d5013f84dd3b479cb46d48e8a4b1f09f04d469f
marvinscheffold <m@marvinscheffold.com>
2022-04-11 16:32:29 +0200

    nach video

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ function add(number1, number2) {
     return number1 + number2;
 }
 
+const add2 = (number1, number2) => number1 + number2;
+
 function getSquaredNumber(number) {
     return number * number;
 }
+
+const getSquaredNumber2 = (number) => number * number;
 
 function getHours() {
     return new Date().getHours();
 }
 
+const getHours2 = () => new Date().getHours();
+
 document.addEventListener("click", function () {
     console.log("Klick!");
 });
+
+document.addEventListener("click", () => console.log("Klick2!"));
--- src/this-in-arrow-funktionen.js
+++ src/this-in-arrow-funktionen.js
@@ -2,14 +2,18 @@ class Student {
     constructor(name) {
         this.name = name;
     }
-
     printNameNormalFunction() {
         setTimeout(function () {
             console.log("Normal", this.name);
         });
     }
-
     printNameArrowFunction() {
         setTimeout(() => console.log("Arrow", this.name));
     }
 }
+
+const marie = new Student("Marie");
+marie.printNameNormalFunction();
+marie.printNameArrowFunction();
+
+
